9eec479af566526f4363a29511f7e5aee5b48c41
Mykyta Tkachenko <n.g.tkachenko@gmail.com>
2026-05-08 15:26:46 +0300

    fix: quickfix migration

--- docker/mysql/migrations/20260506_add_customer_group_price_mapping_to_yml_profile.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `oc_dockercart_import_yml_profile`
-	ADD COLUMN IF NOT EXISTS `customer_group_price_mapping` TEXT NULL AFTER `allow_zero_price`;
--- docker/mysql/migrations/20260507_add_main_price_tag_to_yml_profile.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `oc_dockercart_import_yml_profile`
-	ADD COLUMN IF NOT EXISTS `main_price_tag` VARCHAR(255) NOT NULL DEFAULT 'price' AFTER `customer_group_price_mapping`;
