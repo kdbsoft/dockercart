4f9d919a12d1833022e00af12c560fa98e79e921
Mykyta Tkachenko <n.g.tkachenko@gmail.com>
2026-05-09 08:27:18 +0300

    style: Add login link to footer and update translations
    Updated Ukrainian and Russian language files: normalized array string
    quotes, added login text to footer, updated heading titles and service
    label. Fixed trailing whitespace in footer template.

--- upload/catalog/language/en-gb/common/footer.php
+++ upload/catalog/language/en-gb/common/footer.php
@@ -1,23 +1,25 @@
 <?php
 // Text
-$_['text_information']  = 'Information';
-$_['text_service']      = 'Customer Service';
-$_['text_extra']        = 'Extras';
-$_['text_contact']      = 'Contact Us';
-$_['text_return']       = 'Returns';
-$_['text_sitemap']      = 'Site Map';
-$_['text_manufacturer'] = 'Brands';
-$_['text_voucher']      = 'Gift Certificates';
-$_['text_affiliate']    = 'Affiliate';
-$_['text_special']      = 'Specials';
-$_['text_account']      = 'My Account';
-$_['text_order']        = 'Order History';
-$_['text_wishlist']     = 'Wish List';
-$_['text_newsletter']   = 'Newsletter';
-$_['text_viewed']      = 'Viewed Products';
-$_['text_new_arrivals']   = 'New Arrivals';
-$_['text_sale']           = 'Sale';
-$_['text_catalog']       = 'Catalog';
-$_['text_blog']          = 'Blog';
-$_['text_compare'] = 'Compare';
-$_['text_powered']      = 'Powered By <a href="https://dockercart.net">DockerCart</a><br /> %s &copy; %s';
+$_["text_information"] = "Information";
+$_["text_service"] = "Customer Service";
+$_["text_extra"] = "Extras";
+$_["text_contact"] = "Contact Us";
+$_["text_return"] = "Returns";
+$_["text_sitemap"] = "Site Map";
+$_["text_manufacturer"] = "Brands";
+$_["text_voucher"] = "Gift Certificates";
+$_["text_affiliate"] = "Affiliate";
+$_["text_special"] = "Specials";
+$_["text_account"] = "My Account";
+$_["text_order"] = "Order History";
+$_["text_wishlist"] = "Wish List";
+$_["text_newsletter"] = "Newsletter";
+$_["text_viewed"] = "Viewed Products";
+$_["text_new_arrivals"] = "New Arrivals";
+$_["text_sale"] = "Sale";
+$_["text_catalog"] = "Catalog";
+$_["text_blog"] = "Blog";
+$_["text_compare"] = "Compare";
+$_["text_powered"] =
+    'Powered By <a href="https://dockercart.net">DockerCart</a><br /> %s &copy; %s';
+$_["text_login"] = "Login";
--- upload/catalog/language/ru-ua/common/footer.php
+++ upload/catalog/language/ru-ua/common/footer.php
@@ -1,23 +1,25 @@
 <?php
 
-$_['text_account'] = 'Личный кабинет';
-$_['text_affiliate'] = 'Партнерская программа';
-$_['text_blog'] = 'Блог';
-$_['text_catalog'] = 'Каталог';
-$_['text_compare'] = 'Сравнения';
-$_['text_contact'] = 'Связаться с нами';
-$_['text_extra'] = 'Дополнительные материалы';
-$_['text_information'] = 'Информация';
-$_['text_manufacturer'] = 'Бренды';
-$_['text_newsletter'] = 'Подписка';
-$_['text_viewed'] = 'Просмотренные товары';
-$_['text_new_arrivals'] = 'Новинки';
-$_['text_sale'] = 'Акции';
-$_['text_order'] = 'История заказов';
-$_['text_powered'] = 'Работает на <a href="https://dockercart.net">DockerCart</a><br /> %s © %s';
-$_['text_return'] = 'Возвраты';
-$_['text_service'] = 'Обслуживание клиентов';
-$_['text_sitemap'] = 'Карта сайта';
-$_['text_special'] = 'Специальные предложения';
-$_['text_voucher'] = 'Подарочные сертификаты';
-$_['text_wishlist'] = 'Список желаний';
+$_["text_account"] = "Личный кабинет";
+$_["text_affiliate"] = "Партнерская программа";
+$_["text_blog"] = "Блог";
+$_["text_catalog"] = "Каталог";
+$_["text_compare"] = "Сравнения";
+$_["text_contact"] = "Связаться с нами";
+$_["text_extra"] = "Дополнительные материалы";
+$_["text_information"] = "Информация";
+$_["text_manufacturer"] = "Бренды";
+$_["text_newsletter"] = "Подписка";
+$_["text_viewed"] = "Просмотренные товары";
+$_["text_new_arrivals"] = "Новинки";
+$_["text_sale"] = "Акции";
+$_["text_order"] = "История заказов";
+$_["text_powered"] =
+    'Работает на <a href="https://dockercart.net">DockerCart</a><br /> %s © %s';
+$_["text_return"] = "Возвраты";
+$_["text_service"] = "Покупателям";
+$_["text_sitemap"] = "Карта сайта";
+$_["text_special"] = "Специальные предложения";
+$_["text_voucher"] = "Подарочные сертификаты";
+$_["text_wishlist"] = "Список желаний";
+$_["text_login"] = "Вход";
