ee91d335e84b533ac810f9d3b832d3fedf881717
Mykyta Tkachenko <n.g.tkachenko@gmail.com>
2026-05-09 13:29:57 +0300

    feat: add DockerCart gallery module

---
+++ upload/admin/language/ru-ua/extension/module/dockercart_gallery.php
@@ -0,0 +1,51 @@
+<?php
+// Heading
+$_['heading_title']                = 'DockerCart Галерея';
+$_['heading_title_menu']           = 'Галерея';
+
+// Text
+$_['text_home']                    = 'Главная';
+$_['text_extension']               = 'Расширения';
+$_['text_success']                 = 'Настройки DockerCart Галереи успешно изменены!';
+$_['text_edit']                    = 'Редактировать DockerCart Галерею';
+$_['text_default']                 = 'По умолчанию';
+$_['text_enabled']                 = 'Включено';
+$_['text_disabled']                = 'Отключено';
+$_['text_no_results']              = 'Изображений не найдено.';
+$_['text_add_image']               = 'Добавить изображение';
+$_['text_edit_image']              = 'Редактировать изображение';
+$_['text_image_saved']             = 'Изображение успешно сохранено.';
+$_['text_image_deleted']           = 'Изображение успешно удалено.';
+$_['text_confirm_delete_image']    = 'Удалить это изображение? Файл также будет удалён с сервера.';
+$_['text_gallery_entries']         = 'Изображения галереи';
+$_['text_gallery_module_subtitle'] = 'Фотогалерея с раскладкой masonry и ленивой загрузкой';
+$_['text_gallery_form_subtitle']   = 'Добавить или изменить изображение галереи';
+$_['tab_general']                  = 'Основное';
+$_['text_show_header']             = 'Показывать ссылку в шапке';
+
+// Entry
+$_['entry_status']                 = 'Статус модуля';
+$_['entry_image']                  = 'Изображение';
+$_['entry_sort_order']             = 'Порядок сортировки';
+$_['entry_image_status']           = 'Статус';
+$_['entry_show_header']            = 'Показывать ссылку в верхнем меню';
+
+// Column
+$_['column_image']                 = 'Изображение';
+$_['column_status']                = 'Статус';
+$_['column_sort_order']            = 'Порядок';
+$_['column_action']                = 'Действие';
+
+// Button
+$_['button_save']                  = 'Сохранить';
+$_['button_cancel']                = 'Отмена';
+$_['button_add']                   = 'Добавить';
+$_['button_edit']                  = 'Редактировать';
+$_['button_delete']                = 'Удалить';
+$_['button_add_image']             = 'Добавить изображение';
+
+// Error
+$_['error_permission']             = 'У вас нет прав для изменения DockerCart Галереи!';
+$_['error_form']                   = 'Пожалуйста, проверьте форму.';
+$_['error_image_not_found']        = 'Изображение не найдено.';
+$_['error_image_required']         = 'Изображение обязательно.';
---
+++ upload/catalog/controller/extension/module/dockercart_gallery.php
@@ -0,0 +1,148 @@
+<?php
+declare(strict_types=1);
+
+class ControllerExtensionModuleDockercartGallery extends Controller {
+	public function index() {
+		if (!(int)$this->config->get('module_dockercart_gallery_status')) {
+			$this->response->redirect($this->url->link('common/home'));
+		}
+
+		$this->load->language('extension/module/dockercart_gallery');
+		$this->load->model('extension/module/dockercart_gallery');
+		$this->load->model('tool/image');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$this->document->addStyle('https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css');
+		$this->document->addScript('https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js');
+		$this->document->addScript('catalog/view/theme/dockercart/javascript/dockercart_gallery.js', 'footer');
+
+		$limit = 12;
+		$page = 1;
+
+		$images = $this->model_extension_module_dockercart_gallery->getImages(array(
+			'status' => 1,
+			'sort' => 'sort_order',
+			'order' => 'ASC',
+			'start' => ($page - 1) * $limit,
+			'limit' => $limit
+		));
+
+		$total = $this->model_extension_module_dockercart_gallery->getTotalImages();
+
+		$data['images'] = array();
+		foreach ($images as $image) {
+			$data['images'][] = array(
+				'html' => $this->load->view('extension/module/dockercart_gallery_item', array(
+					'thumb' => 'image/' . $image['image'],
+					'image' => 'image/' . $image['image']
+				))
+			);
+		}
+
+		$data['has_more'] = $total > count($images);
+		$data['total_images'] = $total;
+		$data['load_more_url'] = $this->url->link('extension/module/dockercart_gallery/loadmore');
+		$data['text_load_more'] = $this->language->get('text_load_more');
+		$data['text_no_images'] = $this->language->get('text_no_images');
+		$data['text_images_loaded'] = $this->language->get('text_images_loaded');
+
+		$data['breadcrumbs'] = array();
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home')
+		);
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('extension/module/dockercart_gallery')
+		);
+
+		$data['heading_title'] = $this->language->get('heading_title');
+
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		$this->response->setOutput($this->load->view('extension/module/dockercart_gallery', $data));
+	}
+
+	public function loadmore() {
+		$this->load->language('extension/module/dockercart_gallery');
+		$this->load->model('extension/module/dockercart_gallery');
+		$this->load->model('tool/image');
+
+		$this->response->addHeader('Content-Type: application/json');
+
+		$page = isset($this->request->get['page']) ? (int)$this->request->get['page'] : 2;
+		if ($page < 2) {
+			$page = 2;
+		}
+
+		$limit = 12;
+
+		$images = $this->model_extension_module_dockercart_gallery->getImages(array(
+			'status' => 1,
+			'sort' => 'sort_order',
+			'order' => 'ASC',
+			'start' => ($page - 1) * $limit,
+			'limit' => $limit
+		));
+
+		$total = $this->model_extension_module_dockercart_gallery->getTotalImages();
+
+		$html = '';
+		foreach ($images as $image) {
+			$html .= $this->load->view('extension/module/dockercart_gallery_item', array(
+				'thumb' => 'image/' . $image['image'],
+				'image' => 'image/' . $image['image']
+			));
+		}
+
+		$this->response->setOutput(json_encode(array(
+			'html' => $html,
+			'count' => count($images),
+			'total' => $total
+		)));
+	}
+
+	public function eventFooterBefore(&$route, &$data, &$output) {
+		if (!(int)$this->config->get('module_dockercart_gallery_status')) {
+			return;
+		}
+
+		$this->load->language('extension/module/dockercart_gallery');
+
+		if (!isset($data['informations']) || !is_array($data['informations'])) {
+			$data['informations'] = array();
+		}
+
+		$data['informations'][] = array(
+			'title' => $this->language->get('text_gallery'),
+			'href'  => $this->url->link('extension/module/dockercart_gallery')
+		);
+	}
+
+	public function eventHeaderBefore(&$route, &$data, &$output) {
+		if (!(int)$this->config->get('module_dockercart_gallery_status')) {
+			return;
+		}
+
+		if (!(int)$this->config->get('module_dockercart_gallery_show_header')) {
+			return;
+		}
+
+		$this->load->language('extension/module/dockercart_gallery');
+
+		if (!isset($data['top_informations']) || !is_array($data['top_informations'])) {
+			$data['top_informations'] = array();
+		}
+
+		$data['top_informations'][] = array(
+			'title' => $this->language->get('text_gallery'),
+			'href'  => $this->url->link('extension/module/dockercart_gallery')
+		);
+	}
+}
