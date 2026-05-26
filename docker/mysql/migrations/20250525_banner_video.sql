af55feab74712a46b767f685dbe841379a370bc5
Mykyta Tkachenko <n.g.tkachenko@gmail.com>
2026-05-26 15:43:22 +0300

    fix: quickfix migration

--- docker/mysql/migrations/20250525_banner_video.sql
+++ docker/mysql/migrations/20250525_banner_video.sql
@@ -1,4 +1,4 @@
 -- Add video support columns to oc_banner_image
 ALTER TABLE `oc_banner_image`
-  ADD COLUMN `video_type` varchar(16) NOT NULL DEFAULT '' AFTER `image_portrait`,
-  ADD COLUMN `video` varchar(255) NOT NULL DEFAULT '' AFTER `video_type`;
+  ADD COLUMN IF NOT EXISTS `video_type` varchar(16) NOT NULL DEFAULT '' AFTER `image_portrait`,
+  ADD COLUMN IF NOT EXISTS `video` varchar(255) NOT NULL DEFAULT '' AFTER `video_type`;
