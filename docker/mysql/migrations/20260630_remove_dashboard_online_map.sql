aa201af03bcd59f925db11a299bc97825db41bae
Mykyta Tkachenko <n.g.tkachenko@gmail.com>
2026-06-30 08:55:04 +0300

    fix: use exact setting code and key in migration

--- docker/mysql/migrations/20260630_remove_dashboard_online_map.sql
+++ docker/mysql/migrations/20260630_remove_dashboard_online_map.sql
@@ -2,8 +2,8 @@
 
 DROP TABLE IF EXISTS `oc_customer_online`;
 
-DELETE FROM `oc_setting` WHERE `setting_key` LIKE 'dashboard_online_%';
-DELETE FROM `oc_setting` WHERE `setting_key` LIKE 'dashboard_map_%';
-DELETE FROM `oc_setting` WHERE `setting_key` = 'config_customer_online';
+DELETE FROM `oc_setting` WHERE `code` = 'dashboard_online';
+DELETE FROM `oc_setting` WHERE `code` = 'dashboard_map';
+DELETE FROM `oc_setting` WHERE `code` = 'config' AND `key` = 'config_customer_online';
 
 DELETE FROM `oc_extension` WHERE `type` = 'dashboard' AND `code` IN ('online', 'map');
