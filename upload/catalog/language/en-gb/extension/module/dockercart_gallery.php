a9883154a701c40c721f63a80010e293f4f1e1f4
Mykyta Tkachenko <n.g.tkachenko@gmail.com>
2026-07-06 10:53:30 +0300

    refactor: remove Gallery module

--- upload/catalog/language/en-gb/extension/module/dockercart_gallery.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Heading
-$_['heading_title']       = 'Gallery';
-
-// Text
-$_['text_gallery']        = 'Gallery';
-$_['text_load_more']      = 'Load More';
-$_['text_no_images']      = 'No images in the gallery yet.';
-$_['text_images_loaded']  = 'Images loaded';
