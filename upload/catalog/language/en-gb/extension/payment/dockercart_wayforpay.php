462727b74bcbf83df67ced5c94f3afd0075428ea
Mykyta Tkachenko <n.g.tkachenko@gmail.com>
2026-07-09 08:54:45 +0300

    refactor: remove WayForPay payment module

--- upload/catalog/language/en-gb/extension/payment/dockercart_wayforpay.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_['text_title']            = 'WayForPay';
-$_['button_confirm']        = 'Confirm Order';
-$_['text_loading']          = 'Loading...';
-$_['text_success_title']    = 'Your order has been placed';
-$_['text_success_message']  = 'Your payment is being processed. You will receive a confirmation email shortly.';
-$_['text_order_id']         = 'Order ID';
