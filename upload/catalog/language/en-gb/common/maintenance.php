03a6f82a3a8a1175e80846403c4eb5548429edd7
Mykyta Tkachenko <n.g.tkachenko@gmail.com>
2026-08-16 11:52:58 +0300

    feat(theme): redesign maintenance page with tailwind utilities
    Replace the bootstrap card with a centered tailwind layout, plain message
    text, and status/contact strings across all three locales.

--- upload/catalog/language/en-gb/common/maintenance.php
+++ upload/catalog/language/en-gb/common/maintenance.php
@@ -4,4 +4,6 @@ $_['heading_title']    = 'Maintenance';
 
 // Text
 $_['text_maintenance'] = 'Maintenance';
-$_['text_message']     = '<h1 style="text-align:center;">We are currently performing some scheduled maintenance. <br/>We will be back as soon as possible. Please check back soon.</h1>';
+$_['text_message']     = 'We are currently performing some scheduled maintenance. We will be back as soon as possible. Please check back soon.';
+$_['text_status']      = 'Our team is working to get things back online';
+$_['text_contact']     = 'Contact us';
