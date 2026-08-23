b8c2d39535bf88509ab860582baca160044b131c
Mykyta Tkachenko <n.g.tkachenko@gmail.com>
2026-08-23 19:59:38 +0300

    feat(admin): add SEO URL CSV import and export
    Add export() and import() actions to the seo_url controller with UTF-8 BOM CSV handling, plus list toolbar buttons and per-language strings.

--- upload/admin/language/ru-ua/design/seo_url.php
+++ upload/admin/language/ru-ua/design/seo_url.php
@@ -1,5 +1,9 @@
 <?php
 
+$_['button_cancel'] = 'Отмена';
+$_['button_export_csv'] = 'Экспорт CSV';
+$_['button_import'] = 'Импорт';
+$_['button_import_csv'] = 'Импорт CSV';
 $_['column_action'] = 'Действие';
 $_['column_keyword'] = 'Ключевое слово';
 $_['column_language'] = 'Язык';
@@ -8,23 +12,38 @@ $_['column_store'] = 'Магазин';
 $_['entry_keyword'] = 'Ключевое слово';
 $_['entry_query'] = 'Запрос';
 $_['entry_store'] = 'Магазин';
+$_['error_csv_columns'] = 'Строка %s: ожидалось 4 колонки, получено %s!';
+$_['error_csv_duplicate'] = 'Строка %s: повтор записи для запроса "%s" (магазин %s, язык %s) — уже указана в строке %s!';
+$_['error_csv_empty'] = 'CSV-файл не содержит строк с данными!';
+$_['error_csv_keyword'] = 'Строка %s: пустое ключевое слово (keyword)!';
+$_['error_csv_keyword_conflict'] = 'Строка %s: ключевое слово "%s" уже используется запросом "%s" (магазин %s, язык %s)!';
+$_['error_csv_keyword_length'] = 'Строка %s: ключевое слово длиннее 255 символов!';
+$_['error_csv_language'] = 'Строка %s: неизвестный код языка "%s"!';
+$_['error_csv_query'] = 'Строка %s: пустой запрос (query)!';
+$_['error_csv_query_length'] = 'Строка %s: запрос длиннее 255 символов!';
+$_['error_csv_store'] = 'Строка %s: неизвестный ID магазина "%s"!';
+$_['error_upload'] = 'Внимание: Не удалось загрузить файл!';
 $_['error_exists'] = 'Ключевое слово уже используется!';
 $_['error_keyword'] = 'Ключевое слово должно содержать от 3 до 64 символов!';
 $_['error_permission'] = 'Внимание: у вас нет разрешения на изменение SEO-URL!';
 $_['error_query'] = 'Длина запроса должна составлять от 3 до 64 символов!';
 $_['error_query_exists'] = 'Запрос уже используется!';
+$_['heading_import'] = 'Импорт SEO URL';
 $_['heading_title'] = 'SEO URL';
 $_['text_search_seo_url'] = 'Поиск SEO URL по ключу или запросу';
 $_['text_add'] = 'Добавить SEO-URL';
 $_['text_default'] = 'По умолчанию';
 $_['text_edit'] = 'Редактировать SEO-URL';
 $_['text_filter'] = 'Фильтр';
+$_['text_import_format'] = 'Колонки: store_id, language (код языка), query, keyword. Магазин 0 — магазин по умолчанию. Существующие записи обновляются, новые добавляются. Если хотя бы одна строка невалидна, импорт полностью отменяется.';
+$_['text_import_success'] = 'Успех: Импортировано записей SEO URL: %d (добавлено %d, обновлено %d)!';
 $_['text_list'] = 'Список SEO-URL-адресов';
 $_['text_seo_card'] = 'Ссылки';
 $_['text_keyword'] = 'Не используйте пробелы, вместо них замените пробелы дефисом (-) и убедитесь, что SEO-URL уникален во всей системе.';
 $_['text_seo_preview'] = 'Предпросмотр';
 $_['text_seo_url_base'] = 'yoursite.com/';
 $_['text_none'] = 'Нет';
+$_['text_select_file'] = 'CSV-файл';
 $_['text_success'] = 'Успех: Вы успешно изменили SEO-URL!';
 // Subtitle
 
--- upload/admin/language/uk-ua/design/seo_url.php
+++ upload/admin/language/uk-ua/design/seo_url.php
@@ -1,5 +1,9 @@
 <?php
 
+$_['button_cancel'] = 'Скасувати';
+$_['button_export_csv'] = 'Експорт CSV';
+$_['button_import'] = 'Імпорт';
+$_['button_import_csv'] = 'Імпорт CSV';
 $_['column_action'] = 'Дія';
 $_['column_keyword'] = 'Ключове слово';
 $_['column_language'] = 'Мова';
@@ -8,23 +12,38 @@ $_['column_store'] = 'Магазин';
 $_['entry_keyword'] = 'Ключове слово';
 $_['entry_query'] = 'Запит';
 $_['entry_store'] = 'Магазин';
+$_['error_csv_columns'] = 'Рядок %s: очікувалося 4 колонки, отримано %s!';
+$_['error_csv_duplicate'] = 'Рядок %s: повтор запису для запиту "%s" (магазин %s, мова %s) — вже вказано в рядку %s!';
+$_['error_csv_empty'] = 'CSV-файл не містить рядків з даними!';
+$_['error_csv_keyword'] = 'Рядок %s: порожнє ключове слово (keyword)!';
+$_['error_csv_keyword_conflict'] = 'Рядок %s: ключове слово "%s" уже використовується запитом "%s" (магазин %s, мова %s)!';
+$_['error_csv_keyword_length'] = 'Рядок %s: ключове слово довше за 255 символів!';
+$_['error_csv_language'] = 'Рядок %s: невідомий код мови "%s"!';
+$_['error_csv_query'] = 'Рядок %s: порожній запит (query)!';
+$_['error_csv_query_length'] = 'Рядок %s: запит довший за 255 символів!';
+$_['error_csv_store'] = 'Рядок %s: невідомий ID магазину "%s"!';
+$_['error_upload'] = 'Увага: Не вдалося завантажити файл!';
 $_['error_exists'] = 'Ключове слово вже використовується!';
 $_['error_keyword'] = 'Ключове слово має містити від 3 до 64 символів!';
 $_['error_permission'] = 'Увага: У вас немає дозволу на зміну SEO URL!';
 $_['error_query'] = 'Запит має містити від 3 до 64 символів!';
 $_['error_query_exists'] = 'Запит вже використовується!';
+$_['heading_import'] = 'Імпорт URL-адреси SEO';
 $_['heading_title'] = 'URL-адреса SEO';
 $_['text_search_seo_url'] = 'Пошук SEO URL за ключем або запитом';
 $_['text_add'] = 'Додати URL-адресу SEO';
 $_['text_default'] = 'За замовчуванням';
 $_['text_edit'] = 'Редагувати URL-адресу SEO';
 $_['text_filter'] = 'Фільтр';
+$_['text_import_format'] = 'Колонки: store_id, language (код мови), query, keyword. Магазин 0 — магазин за замовчуванням. Існуючі записи оновлюються, нові додаються. Якщо хоча б один рядок невалідний, імпорт повністю скасовується.';
+$_['text_import_success'] = 'Успіх: Імпортовано записів SEO URL: %d (додано %d, оновлено %d)!';
 $_['text_list'] = 'Список URL-адрес SEO';
 $_['text_seo_card'] = 'Посилання';
 $_['text_keyword'] = 'Не використовуйте пробіли, натомість замініть їх на - та переконайтеся, що SEO-URL є глобально унікальним.';
 $_['text_seo_preview'] = 'Попередній перегляд';
 $_['text_seo_url_base'] = 'yoursite.com/';
 $_['text_none'] = 'Немає';
+$_['text_select_file'] = 'CSV-файл';
 $_['text_success'] = 'Успіх: Ви змінили SEO-URL!';
 // Subtitle
 
