9d76414f46bdb1b27fa1413c0536517b3aa8e1f6
Mykyta Tkachenko <n.g.tkachenko@gmail.com>
2026-08-30 10:29:46 +0300

    refactor(lang): shorten text_all_categories label across locales

--- upload/catalog/language/uk-ua/extension/module/bestseller.php
+++ upload/catalog/language/uk-ua/extension/module/bestseller.php
@@ -13,4 +13,4 @@ $_['text_instock'] = 'В наявності';
 $_['text_preorder'] = 'Передзамовлення';
 $_['text_out_of_stock'] = 'Немає в наявності';
 $_['text_other'] = 'Інші';
-$_['text_all_categories'] = 'Усі категорії';
+$_['text_all_categories'] = 'Усі';
